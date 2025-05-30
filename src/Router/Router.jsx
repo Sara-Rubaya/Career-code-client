caf46cafe73a0c1b894f0899f0922b9362addae5
Sara-Rubaya <sararubaya4800@gmail.com>
2025-05-30 12:52:29 +0600

    added register, signIn, signOut

---
+++ src/Router/Router.jsx
@@ -0,0 +1,30 @@
+import {
+  createBrowserRouter,
+} from "react-router";
+import RootLayout from "../Layouts/RootLayout";
+import Home from "../Pages/Home/Home";
+import Register from "../Pages/Register/Register";
+import SignIn from "../Pages/SignIn/SignIn";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component:RootLayout ,
+    children:[
+        {
+            index:true,
+            Component:Home,
+        },
+        {
+          path: 'register',
+          Component:Register,
+        },
+        {
+          path:'signIn',
+          Component: SignIn
+        }
+    ]
+  },
+]);
+
+export default router;
